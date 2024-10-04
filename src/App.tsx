e662c6cac42fac44e0093d0cf2155baca38e417c
Brendan <bgmcmullen@comcast.net>
2024-10-03 19:40:37 -0700

    added instructions state

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import Button from '@mui/material/Button';
 
 import './App.css'
 
+
+
 function App() {
+  const [intructions, setIntructions] = useState('Instructions');
 
   return (
     <>
       <div>
         <Button variant="contained">Start</Button>
+        <p>Instructions: {intructions}</p>
       </div>
     </>
   )
